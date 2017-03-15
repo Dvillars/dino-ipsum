12c82210ee2481ae10217703bb44085b632ad3a0
Derek Villars and Jiwon Kang <jiwon@>
2017-03-15 11:18:38 -0700

    add jquery get functions

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-  $('#dino-form').submit(function(event) {
+  $('#dino-form').click(function(event) {
     event.preventDefault();
 
-    
+    var words = parseInt($('#dino-words').val());
+    var paragraphs = parseInt($('#dino-paragraphs').val());
+
+    $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=' + words + '&words=' + paragraphs)
+    .then(function(json) {
+        $('#display-result').text(json);
+    })
+    .fail(function() {
+        console.log('Where did all the dinosaurs go?');
+    });
   });
 });
